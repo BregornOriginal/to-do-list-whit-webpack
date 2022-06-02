5367bb2882e51295053da6f6b60f8f803b3909a0
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-06-02 01:11:56 -0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 
 const toDoTasks = [{
-  description: "Wash the dishes",
+  description: 'Wash the dishes',
   completed: true,
-  index: 1
+  index: 1,
 },
 {
-  description: "Complete To Do list project",
+  description: 'Complete To Do list project',
   completed: true,
-  index: 2
-}]
+  index: 2,
+}];
 
 const createList = () => {
   const $listContainer = document.getElementById('list-container');
   for (let i = 0; i < toDoTasks.length; i += 1) {
-
     const $taskContainer = document.createElement('div');
     $taskContainer.classList.add('task-container');
     $taskContainer.classList.add('input-group');
@@ -41,7 +40,7 @@ const createList = () => {
     $placeHolderTask.classList.add('form-control');
     $placeHolderTask.setAttribute('type', 'text');
     $placeHolderTask.setAttribute('placeholder', `${toDoTasks[i].description}`);
-    $placeHolderTask.setAttribute('aria-label', 'Text input with checkbox')
+    $placeHolderTask.setAttribute('aria-label', 'Text input with checkbox');
     $taskContainer.appendChild($placeHolderTask);
 
     const $dotsOption = document.createElement('a');
