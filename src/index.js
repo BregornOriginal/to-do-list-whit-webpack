81989f58ab3bc447487ddbe2fb65e65134c49c89
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-06-03 15:07:26 -0300

    Added functions to remove an especific index list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 
+import { updateIndex, updateElementId } from './modules/update-index.js';
+
 class Task {
   constructor(description, completed, index) {
     this.description = description;
@@ -13,32 +15,27 @@ class Task {
 const $listContainer = document.getElementById('list-container');
 const $textNewTask = document.getElementById('textNewTask');
 
-const toDoTasks = [];
+let toDoTasks = [];
 
 const addTask = () => {
   const $taskContainer = document.createElement('div');
-  $taskContainer.classList.add('task-container');
-  $taskContainer.classList.add('input-group');
-  $taskContainer.classList.add('mb-3');
+  $taskContainer.setAttribute('id', toDoTasks.length + 1);
+  $taskContainer.classList.add('task-container', 'input-group', 'mb-3');
   $listContainer.appendChild($taskContainer);
 
   const $checkBoxContainer = document.createElement('div');
-  $checkBoxContainer.classList.add('input-group-text');
-  $checkBoxContainer.classList.add('bg-light');
-  $checkBoxContainer.classList.add('text-dark');
+  $checkBoxContainer.classList.add('input-group-text', 'bg-light', 'text-dark');
   $taskContainer.appendChild($checkBoxContainer);
 
   const $checkBox = document.createElement('input');
-  $checkBox.setAttribute('id', 'checkbox')
-  $checkBox.classList.add('form-check-input');
-  $checkBox.classList.add('mt-0');
+  $checkBox.setAttribute('id', 'checkbox');
+  $checkBox.classList.add('form-check-input', 'mt-0');
   $checkBox.setAttribute('type', 'checkbox');
   $checkBox.setAttribute('value', '');
   $checkBox.setAttribute('aria-label', 'Checkbox for following text input');
   $checkBoxContainer.appendChild($checkBox);
 
   $checkBox.addEventListener('click', () => {
-    $checkBox.parentElement.nextElementSibling.classList.toggle('checked');
     $checkBox.parentElement.nextElementSibling.classList.toggle('line-through');
     $checkBox.parentElement.parentElement.lastElementChild.classList.toggle('trash-active');
     $checkBox.parentElement.parentElement.lastElementChild.previousElementSibling.classList.toggle('hidden');
@@ -52,11 +49,7 @@ const addTask = () => {
   $taskContainer.appendChild($placeHolderTask);
 
   const $dotsOption = document.createElement('a');
-  $dotsOption.classList.add('edit-button');
-  $dotsOption.classList.add('bi');
-  $dotsOption.classList.add('bi-three-dots-vertical');
-  $dotsOption.classList.add('bg-secondary');
-  $dotsOption.classList.add('text-white');
+  $dotsOption.classList.add('edit-button', 'bi', 'bi-three-dots-vertical', 'bg-secondary', 'text-white');
   $taskContainer.appendChild($dotsOption);
 
   $dotsOption.addEventListener('click', () => {
@@ -64,13 +57,19 @@ const addTask = () => {
   })
 
   const $trash = document.createElement('i');
-  $trash.classList.add('hidden');
-  $trash.classList.add('trash-icon');
-  $trash.classList.add('fa-solid');
-  $trash.classList.add('fa-trash-can');
+  $trash.classList.add('hidden', 'trash-icon', 'fa-solid', 'fa-trash-can');
   $taskContainer.appendChild($trash);
 
-  const object = new Task($textNewTask.value, false, toDoTasks.length)
+  $trash.addEventListener('click', () => {
+    $trash.parentElement.remove();
+    const newToDoTask = toDoTasks.filter(task => task.index !== parseInt($trash.parentElement.id, 10));
+    toDoTasks = [...newToDoTask];
+    updateIndex(toDoTasks);
+    updateElementId(toDoTasks);
+    localStorage.setItem('list', JSON.stringify(toDoTasks));
+  })
+
+  const object = new Task($textNewTask.value, false, toDoTasks.length + 1);
   toDoTasks.push(object);
   localStorage.setItem('list', JSON.stringify(toDoTasks));
 };
@@ -85,20 +84,17 @@ $textNewTask.addEventListener('keypress', e => {
 
 const editText = ($checkBoxContainer, toDo) => {
   const $newPlaceHolderTask = document.createElement('input');
-  $newPlaceHolderTask.classList.add('checked');
-  $newPlaceHolderTask.classList.add('form-control');
+  $newPlaceHolderTask.classList.add('checked', 'form-control');
   $newPlaceHolderTask.setAttribute('type', 'text');
-  console.log($checkBoxContainer)
-  console.log(toDo)
   $newPlaceHolderTask.value = toDo.textContent;
   $newPlaceHolderTask.setAttribute('aria-label', 'Text input with checkbox');
   $checkBoxContainer.replaceChild($newPlaceHolderTask, toDo);
   $newPlaceHolderTask.addEventListener('keypress', e => {
     if (e.key === 'Enter') {
-      const $allTaskContainers = document.querySelectorAll('task-container');
-      const localData = JSON.parse(localStorage.getItem('list'));
+      const $allTaskContainers = document.querySelectorAll('.task-container');
+      let localData = JSON.parse(localStorage.getItem('list'));
       for (let i = 0; i < $allTaskContainers.length; i += 1) {
-        if ($allTaskContainers[i].classList.contains('checked')) {
+        if ($allTaskContainers[i].firstChild.nextSibling.classList.contains('checked')) {
           localData[i].description = $newPlaceHolderTask.value;
           localStorage.setItem('list', JSON.stringify(localData));
         }
@@ -106,6 +102,7 @@ const editText = ($checkBoxContainer, toDo) => {
       $newPlaceHolderTask.classList.remove('checked');
       $checkBoxContainer.replaceChild(toDo, $newPlaceHolderTask);
       toDo.textContent = $newPlaceHolderTask.value;
+      toDoTasks = [...localData];
     }
   })
-}
+};
